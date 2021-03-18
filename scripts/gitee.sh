d4fd739daffe13afb46de7c164dddd1d29403a54
jack <loveminimal@163.com>
2021-03-18 16:51:35 +0800

    Deploy to gitee.

---
+++ scripts/gitee.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# -------------------
+# Deploy posts to `loveminimal.github.io`
+# -------------------
+
+if [ -d "public" ]
+then
+    cp -r "public" "../.temp"
+    cd "../.temp"
+    pwd
+    git init
+    git add .
+    git commit -m "Posts update."
+    git remote add origin https://gitee.com/loveminimal/loveminimal.git
+    git push -f origin master:main
+    cd ..
+    rm -rf ".temp"
+    cd "site"
+fi
