0231a04d73ee332146bd5a4e01a128f263b6d585
jack <loveminimal@163.com>
2021-07-08 11:51:10 +0800

    Active jsDlivr.

--- public/themes/main.js
+++ public/themes/main.js
@@ -1,6 +1,8 @@
 'use strict';
-import './assets/jquery.min.js';
-import './assets/darkreader.min.js';
+// import './assets/jquery.min.js';
+// import './assets/darkreader.min.js';
+import 'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js';
+import 'https://cdn.jsdelivr.net/npm/darkreader@4.9.34/darkreader.min.js';
 import userconfig from '../user.config.js';
 import {
     isHome,
